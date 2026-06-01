9c6681bd847ac57282bc8a356b4563ffa53ba235
jubidubab-chunza <jubidubabchunza@gmail.com>
2026-06-02 06:08:43 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 1.73 ms, Memory: 4 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    for (int r=0; r<n; r++) {
+        for (int c=0; c<r+1; c++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
