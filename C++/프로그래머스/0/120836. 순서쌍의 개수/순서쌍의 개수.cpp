6ab267b749d6a9d2ab3c11dac915f2224e5bf82a
jubidubab-chunza <jubidubabchunza@gmail.com>
2026-06-04 00:13:33 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    bool flag = false;
+    for (int i=1; i*i<=n; i++) {
+        if (n%i == 0) {
+            answer += i*i==n?1:2;
+        }
+    }
+    return answer;
+}
