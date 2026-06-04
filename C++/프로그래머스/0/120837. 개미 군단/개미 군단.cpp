0b6194033ceee80d0d49056628250ccd836c826f
jubidubab-chunza <jubidubabchunza@gmail.com>
2026-06-04 22:24:19 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = hp/5;
+    hp = hp%5;
+    
+    answer += hp/3;
+    hp = hp%3;
+    
+    answer += hp;
+    
+    return answer;
+}
