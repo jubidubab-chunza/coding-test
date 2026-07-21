fe782f1baf9e35ee7b8d1bb5156c6cf72cdbebf7
jubidubab-chunza <jubidubabchunza@gmail.com>
2026-07-22 07:50:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for (char c: my_string) {
+        if (c >= 'a') {
+            answer += c - ('a'-'A');
+        }
+        else {
+            answer += c + ('a'-'A');
+        }
+    }
+    return answer;
+}
