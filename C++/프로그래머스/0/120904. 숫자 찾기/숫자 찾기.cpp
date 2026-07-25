b5e44b70567c4cc4c0406def66c2419dc40113c6
jubidubab-chunza <jubidubabchunza@gmail.com>
2026-07-25 13:30:11 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <string>
+
+using namespace std;
+
+int solution(int num, int k) {
+    string s = to_string(num);
+    
+    for (int i = 0; i<s.size(); i++) {
+        if (s[i] == k + '0') return i+1;
+    }
+    return -1;
+}
